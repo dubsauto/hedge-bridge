09e0244f98c77d0c869485c0cf3b27f7078388d3
dubsauto <dubstechstandard@gmail.com>
2026-04-30 18:57:05 +0100

    start ...

--- run_listener.py
+++ run_listener.py
@@ -2,7 +2,9 @@ import asyncio
 from hedgebridge.listener_manager import listener_manager
 
 async def main():
+    print("🔥 SCRIPT STARTED")
     await listener_manager.start()
 
 if __name__ == "__main__":
     asyncio.run(main())
+
